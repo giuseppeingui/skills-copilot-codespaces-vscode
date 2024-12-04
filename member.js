d70e719b20f00b0826d7c0e6a0a495df6e44becb
giuseppeingui <39086472+giuseppeingui@users.noreply.github.com>
2024-12-04 15:26:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMembers() {
+    var members = [{
+        name: 'John Doe',
+        skills: ['JavaScript', 'Python', 'Node.js']
+    }, {
+        name: 'Jane Doe',
+        skills: ['JavaScript', 'PHP', 'Ruby']
+    }];
+    return members;
+}
